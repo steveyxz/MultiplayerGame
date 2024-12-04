0ad5b5938888f5388e4026dd1b21515f1e823dce
Steven Zhu <65844236+steveyxz@users.noreply.github.com>
2024-12-05 08:23:48 +1100

    what

---
+++ Assets/Scripts/Client/StatDisplay.cs
@@ -0,0 +1,48 @@
+﻿using Client.Stats;
+using Global;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Client
+{
+    public class StatDisplay : NetworkBehaviour
+    {
+        
+        [SerializeField] private Slider _healthSlider;
+        [SerializeField] private Slider _resourceSlider;
+        
+        private ClientStatHandler _clientStats;
+        
+        public override void OnNetworkSpawn()
+        {
+            _clientStats = GetComponent<ClientStatHandler>();
+            _clientStats.OnHealthChanged += OnHealthChanged;
+            _clientStats.OnResourceChanged += OnResourceChanged;
+        }
+
+        private void OnResourceChanged(float oldvalue, float newvalue)
+        {
+            var maxResource = _clientStats.GetAggregatedValue(Stat.MaxResource);
+            if (maxResource == 0)
+            {
+                return;
+            }
+            _resourceSlider.value = newvalue / maxResource;
+            Debug.Log("Max Resource: " + maxResource);
+            Debug.Log("Resource amount: " + newvalue);
+        }
+
+        private void OnHealthChanged(float oldvalue, float newvalue)
+        {
+            var maxHealth = _clientStats.GetAggregatedValue(Stat.MaxHp);
+            if (maxHealth == 0)
+            {
+                return;
+            }
+            _healthSlider.value = newvalue / maxHealth;
+            Debug.Log("Max Health: " + maxHealth);
+            Debug.Log("Health amount: " + newvalue);
+        }
+    }
+}
